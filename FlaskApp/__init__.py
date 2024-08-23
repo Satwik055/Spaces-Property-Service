40d76bbcbbdc195c5607c25829096c29825b4a79
7week <satwikkumar055@gmail.com>
2024-08-24 01:34:53 +0530

    Cleaned code

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -4,36 +4,38 @@ import psycopg2
 
 # https://spaces-property-service.azurewebsites.net/v1/property/3
 
-connection = psycopg2.connect(
-    host="aws-0-ap-south-1.pooler.supabase.com",
-    database="postgres",
-    user="postgres.fayafjrwupqupjltsdeg",
-    password="@Satwikkr055"
-)
 
+DB_CONFIG = {
+    'host': "aws-0-ap-south-1.pooler.supabase.com",
+    'database': "postgres",
+    'user': "postgres.fayafjrwupqupjltsdeg",
+    'password': "@Satwikkr055"
+}
+
+connection = psycopg2.connect(**DB_CONFIG)
 cursor = connection.cursor()
-    
 app = Flask(__name__)
 
 property_types = ["workspace", "lounge"]
+
 
 @app.route('/v1/property', methods=['GET'])
 def get_properties_by_type():
     property_type = request.args.get('type')
 
-    if property_type not in property_types:
-        return jsonify({"error": "Invalid property type"}), 400
-
-    if not property_type:
-        return jsonify({"error": "Property type is required."}), 400
-
     try:
+        if property_type not in property_types:
+            raise Exception("Invalid property type")
+
+        if not property_type:
+            raise Exception("Property type is required")
+
         cursor.callproc('get_properties_by_type', [property_type])
         response = cursor.fetchone()
 
         if response is None:
-            return jsonify({"error": f"No property available of {property_type} type"}), 400
-    
+            raise Exception(f"No property available of {property_type} type")
+
         else:
             columns = [desc[0] for desc in cursor.description]
             response_dict = dict(zip(columns, response))
@@ -42,11 +44,12 @@ def get_properties_by_type():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
 @app.route('/v1/property/<int:property_id>', methods=['GET'])
 def get_property_by_id(property_id):
     try:
         cursor.callproc('get_property_by_id', [property_id])
-        
+
         response = cursor.fetchone()
 
         if response is None:
@@ -58,13 +61,12 @@ def get_property_by_id(property_id):
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-    
+
+
 @app.route('/hello', methods=['GET'])
 def hello():
-    return "Hello World Jalzeera !"
-
+    return "Hello World"
 
 
 if __name__ == '__main__':
     app.run()
-
